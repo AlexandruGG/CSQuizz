090cfeddb685d519aa07b552de1ad2051ad8caa5
Alex Gidei <alex.gidei@gmail.com>
2018-03-09 15:50:56 +0000

    Improved Resource IDs Improved Code Formatting Improved Answer Checking For EdiText Questions

--- app/src/main/java/com/example/android/csquizz/MainActivity.java
+++ app/src/main/java/com/example/android/csquizz/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //Views used to manage scrolling and focus
+    //View used to manage scrolling
     ScrollView quizView;
 
     @Override
@@ -28,9 +28,9 @@ public class MainActivity extends AppCompatActivity {
      * This method scrolls down and focuses on a dummy view when "Begin" button is pressed
      ***/
     public void scrollDown(View v) {
-        findViewById(R.id.user_name).clearFocus();
-        findViewById(R.id.focus).requestFocus();
-        quizView.smoothScrollTo(0, findViewById(R.id.question2).getTop());
+        findViewById(R.id.edit_user_name).clearFocus();
+        findViewById(R.id.view_focus).requestFocus();
+        quizView.smoothScrollTo(0, findViewById(R.id.xml_question2).getTop());
     }
 
     /***
@@ -53,41 +53,38 @@ public class MainActivity extends AppCompatActivity {
         String nameMessage = getString(R.string.name_input, userName);
         String finalMessage = " " + nameMessage + youScored + " " + scoreTotal + "/5.";
 
-        if (scoreTotal == 5 || scoreTotal == 4)
-            makeToastText(R.string.high_score, finalMessage);
-        else if (scoreTotal == 0 || scoreTotal == 1 || scoreTotal == 2)
-            makeToastText(R.string.low_score, finalMessage);
-        else
-            makeToastText(R.string.med_score, finalMessage);
+        if (scoreTotal == 5 || scoreTotal == 4) makeToastText(R.string.high_score, finalMessage);
+        else if (scoreTotal == 0 || scoreTotal == 1 || scoreTotal == 2) makeToastText(R.string.low_score, finalMessage);
+        else makeToastText(R.string.med_score, finalMessage);
 
         //Disables input for name field
-        findViewById(R.id.user_name).setEnabled(false);
+        findViewById(R.id.edit_user_name).setEnabled(false);
 
         //Disables input for question 1
-        findViewById(R.id.q1_answer).setEnabled(false);
+        findViewById(R.id.edit_q1_answer).setEnabled(false);
 
         //Disables input for question 2
-        for (int i = 0; i < ((RadioGroup) findViewById(R.id.q2_radioGroup)).getChildCount(); i++) {
-            ((RadioGroup) findViewById(R.id.q2_radioGroup)).getChildAt(i).setEnabled(false);
+        for (int i = 0; i < ((RadioGroup) findViewById(R.id.radio_group_q2_answers)).getChildCount(); i++) {
+            ((RadioGroup) findViewById(R.id.radio_group_q2_answers)).getChildAt(i).setEnabled(false);
         }
         //Disables input for question 3
-        findViewById(R.id.q3_answer).setEnabled(false);
+        findViewById(R.id.edit_q3_answer).setEnabled(false);
 
         //Disables input for question 4
-        ((CheckBox) findViewById(R.id.q4_answer1)).setEnabled(false);
-        ((CheckBox) findViewById(R.id.q4_answer2)).setEnabled(false);
-        ((CheckBox) findViewById(R.id.q4_answer3)).setEnabled(false);
-        ((CheckBox) findViewById(R.id.q4_answer4)).setEnabled(false);
+        ((CheckBox) findViewById(R.id.check_q4a1_better_balanced)).setEnabled(false);
+        ((CheckBox) findViewById(R.id.check_q4a2_faster_insertion)).setEnabled(false);
+        ((CheckBox) findViewById(R.id.check_q4a3_faster_lookup)).setEnabled(false);
+        ((CheckBox) findViewById(R.id.check_q4a4_more_data)).setEnabled(false);
 
         //Disables input for question 5
-        findViewById(R.id.q5_answer).setEnabled(false);
+        findViewById(R.id.edit_q5_answer).setEnabled(false);
     }
 
     /***
      * This method gets the user's name (introduced in the first EditText object)
      ***/
-    public String getUserName() {
-        return ((EditText) findViewById(R.id.user_name)).getText().toString();
+    private String getUserName() {
+        return ((EditText) findViewById(R.id.edit_user_name)).getText().toString();
     }
 
     /***
@@ -98,52 +95,52 @@ public class MainActivity extends AppCompatActivity {
         String nameMessage = getString(R.string.name_input, userName);
 
         //Scroll to beginning of quiz
-        quizView.smoothScrollTo(0, findViewById(R.id.title).getTop());
+        quizView.smoothScrollTo(0, findViewById(R.id.text_title).getTop());
 
         //Reset Name Field
-        EditText nameField = findViewById(R.id.user_name);
+        EditText nameField = findViewById(R.id.edit_user_name);
         nameField.setEnabled(true);
 
         //Reset Question 1
-        EditText q1Input = findViewById(R.id.q1_answer);
+        EditText q1Input = findViewById(R.id.edit_q1_answer);
         q1Input.setText("");
         q1Input.setEnabled(true);
         q1Input.clearFocus();
 
         //Reset Question 2
-        RadioGroup q2RadioGroup = findViewById(R.id.q2_radioGroup);
+        RadioGroup q2RadioGroup = findViewById(R.id.radio_group_q2_answers);
         q2RadioGroup.clearCheck();
         for (int i = 0; i < q2RadioGroup.getChildCount(); i++) {
             q2RadioGroup.getChildAt(i).setEnabled(true);
         }
 
         //Reset Question 3
-        EditText q3Input = findViewById(R.id.q3_answer);
+        EditText q3Input = findViewById(R.id.edit_q3_answer);
         q3Input.setText("");
         q3Input.setEnabled(true);
         q3Input.clearFocus();
 
         //Reset Question 4
-        ((CheckBox) findViewById(R.id.q4_answer1)).setEnabled(true);
-        ((CheckBox) findViewById(R.id.q4_answer1)).setChecked(false);
+        ((CheckBox) findViewById(R.id.check_q4a1_better_balanced)).setEnabled(true);
+        ((CheckBox) findViewById(R.id.check_q4a1_better_balanced)).setChecked(false);
 
-        ((CheckBox) findViewById(R.id.q4_answer2)).setEnabled(true);
-        ((CheckBox) findViewById(R.id.q4_answer2)).setChecked(false);
+        ((CheckBox) findViewById(R.id.check_q4a2_faster_insertion)).setEnabled(true);
+        ((CheckBox) findViewById(R.id.check_q4a2_faster_insertion)).setChecked(false);
 
-        ((CheckBox) findViewById(R.id.q4_answer3)).setEnabled(true);
-        ((CheckBox) findViewById(R.id.q4_answer3)).setChecked(false);
+        ((CheckBox) findViewById(R.id.check_q4a3_faster_lookup)).setEnabled(true);
+        ((CheckBox) findViewById(R.id.check_q4a3_faster_lookup)).setChecked(false);
 
-        ((CheckBox) findViewById(R.id.q4_answer4)).setEnabled(true);
-        ((CheckBox) findViewById(R.id.q4_answer4)).setChecked(false);
+        ((CheckBox) findViewById(R.id.check_q4a4_more_data)).setEnabled(true);
+        ((CheckBox) findViewById(R.id.check_q4a4_more_data)).setChecked(false);
 
         //Reset Question 5
-        EditText q5Input = findViewById(R.id.q5_answer);
+        EditText q5Input = findViewById(R.id.edit_q5_answer);
         q5Input.setText("");
         q5Input.setEnabled(true);
         q5Input.clearFocus();
 
         //Set focus on dummy view
-        findViewById(R.id.focus).requestFocus();
+        findViewById(R.id.view_focus).requestFocus();
 
         //Show Toast
         Toast.makeText(this, "Give it another go, " + nameMessage + "!", Toast.LENGTH_LONG).show();
@@ -154,33 +151,31 @@ public class MainActivity extends AppCompatActivity {
      * The methods below get the user's answers and compare them to the correct answers
      ***/
     public int answerQ1() {
-        String q1Answer = ((EditText) findViewById(R.id.q1_answer)).getText().toString();
+        String q1Answer = ((EditText) findViewById(R.id.edit_q1_answer)).getText().toString().trim();
         return q1Answer.compareToIgnoreCase(getString(R.string.q1_answer)) == 0 ? 1 : 0;
     }
 
     public int answerQ2() {
-        return ((RadioButton) findViewById(R.id.a2_radio)).isChecked() ? 1 : 0;
+        return ((RadioButton) findViewById(R.id.radio_a2)).isChecked() ? 1 : 0;
     }
 
     public int answerQ3() {
-        String q3Answer = ((EditText) findViewById(R.id.q3_answer)).getText().toString();
+        String q3Answer = ((EditText) findViewById(R.id.edit_q3_answer)).getText().toString().trim();
         return q3Answer.compareToIgnoreCase(getString(R.string.q3_answer)) == 0 ? 1 : 0;
     }
 
     public int answerQ4() {
-        boolean q4Answer1IsChecked = ((CheckBox) findViewById(R.id.q4_answer1)).isChecked();
-        boolean q4Answer2IsChecked = ((CheckBox) findViewById(R.id.q4_answer2)).isChecked();
-        boolean q4Answer3IsChecked = ((CheckBox) findViewById(R.id.q4_answer3)).isChecked();
-        boolean q4Answer4IsChecked = ((CheckBox) findViewById(R.id.q4_answer4)).isChecked();
+        boolean q4Answer1IsChecked = ((CheckBox) findViewById(R.id.check_q4a1_better_balanced)).isChecked();
+        boolean q4Answer2IsChecked = ((CheckBox) findViewById(R.id.check_q4a2_faster_insertion)).isChecked();
+        boolean q4Answer3IsChecked = ((CheckBox) findViewById(R.id.check_q4a3_faster_lookup)).isChecked();
+        boolean q4Answer4IsChecked = ((CheckBox) findViewById(R.id.check_q4a4_more_data)).isChecked();
 
-        if (q4Answer1IsChecked && q4Answer2IsChecked && q4Answer3IsChecked && !q4Answer4IsChecked)
-            return 1;
-        else
-            return 0;
+        if (q4Answer1IsChecked && q4Answer2IsChecked && q4Answer3IsChecked && !q4Answer4IsChecked) return 1;
+        else return 0;
     }
 
     public int answerQ5() {
-        String q5Answer = ((EditText) findViewById(R.id.q5_answer)).getText().toString();
+        String q5Answer = ((EditText) findViewById(R.id.edit_q5_answer)).getText().toString().trim();
         return q5Answer.compareToIgnoreCase(getString(R.string.q5_answer)) == 0 ? 1 : 0;
     }
 
